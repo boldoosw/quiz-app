506917b90b04e13d933a1362fbd49dda66a5e2ee
ganbold <boldoosw@gmail.com>
2024-01-09 21:40:52 +0800

    climov logo oruullaa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {/* <h1>This is the main layout</h1> */}
         <Navbar />
-        <div className="container px-5 py-24 mx-auto flex justify-center flex-wrap bg-gray-100">
+        <div className="container px-5 py-5 mx-auto flex justify-center flex-wrap bg-gray-100">
           {children}
         </div>
 
