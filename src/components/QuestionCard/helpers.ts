dd91904749d4ec9c39df244472c5211a8beabf30
ganbold <boldoosw@gmail.com>
2024-01-09 21:23:20 +0800

    ungu tsenher bolgow

--- src/components/QuestionCard/helpers.ts
+++ src/components/QuestionCard/helpers.ts
@@ -3,7 +3,7 @@ export const getBGColor = (userAnswer: string | undefined, correctAnswer: string
 
   // if ((isAnswerCorrect === true && answer === userAnswer) || (isAnswerCorrect === false && answer === correctAnswer))
     if (answer === userAnswer)
-    return 'bg-[#55AC78] text-white';
+    return 'bg-blue-500 text-white';
 
   // if (isAnswerCorrect === false && answer === userAnswer) return 'bg-[#AC5050] text-white';
 
