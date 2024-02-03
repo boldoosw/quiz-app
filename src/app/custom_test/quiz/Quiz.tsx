fb236fe6cb6cb5e5f438f9297130ad10efdefa6d
ganbold <boldoosw@gmail.com>
2024-02-03 09:17:37 +0800

    Card font bagasgav

--- src/app/custom_test/quiz/Quiz.tsx
+++ src/app/custom_test/quiz/Quiz.tsx
@@ -15,34 +15,38 @@ type Props = {
 // eslint-disable-next-line react-hooks/rules-of-hooks
 
 const Quiz = ({ questions }: Props) => {
-  // const q = questions[0];
+  const [currentQuestionIndex, setCurrentQuestionIndex] = React.useState(0);
+  const [userAnswers, setUserAnswers] = React.useState<Record<number, string>>(
+    {}
+  );
+  const handleOnAnswerClick = (
+    answer: string,
+    answer_val: string,
+    currentQuestionIndex: number
+  ) => {
+    setUserAnswers((prev) => ({ ...prev, [currentQuestionIndex]: answer }));
+
+    userAnswers[currentQuestionIndex] = answer;
+
+    console.log(answer);
+    console.log(currentQuestionIndex);
+  };
 
   return (
     <div className="md:grid grid-cols-2 gap-2">
-      {questions.map(function (item, i) {
-        console.log("test");
+      {questions.map(function (question, i) {
         return (
           <CustomCard
             key={i}
-            question={item}
-            radioHandler={function (): void {
-              throw new Error("Function not implemented.");
-            }}
+            question={question}
+            currentQuestionKey={i}
+            userAnswer={userAnswers[currentQuestionIndex]}
+            onChange={handleOnAnswerClick}
           />
         );
       })}
     </div>
   );
-
-  // <div>
-  //   <ul>
-  //     {questions.map(function (item, i) {
-  //       console.log("test");
-  //       return <li key={i}>Test</li>;
-  //     })}
-  //   </ul>
-  // </div>;
-  // questions.map((question: Question) => ());
 };
 
 export default Quiz;
--- src/components/QuestionCard/CustomCard.tsx
+++ src/components/QuestionCard/CustomCard.tsx
@@ -1,49 +1,58 @@
 import React from "react";
 
 import { Question } from "@/types/quiz";
+import { getBGColor } from "./helpers";
 const { useEffect, useState } = React;
 type Props = {
   question: Question;
-
-  radioHandler: () => void;
+  currentQuestionKey: number;
+  userAnswer: string | undefined;
+  onChange: (
+    answer: string,
+    answer_val: string,
+    currentQuestionIndex: number
+  ) => void;
 };
 
-const CustomCard: React.FC<Props> = ({ question, radioHandler }) => {
+const CustomCard: React.FC<Props> = ({
+  question,
+  currentQuestionKey,
+  userAnswer,
+  onChange,
+}) => {
   const [selected, setSelected] = useState("");
 
-  // Destructure the id from the clicked element
-  // and set the state with it
-  function handleChange(e: { target: { id: any } }) {
-    const { id } = e.target;
-    setSelected(id);
-    console.log(id);
-  }
-  // Log the state after its been updated
-  useEffect(() => {
-    if (selected) console.log(selected);
-  }, [selected]);
   return (
     <div className="md:w-full gap-6 md:grid-cols-2 m-2">
       <div
-        id=""
+        id={question.question}
         className="bg-sky-100 flex
         flex-row justify-between items-center cursor-pointer 
             select-none  max-w-[400] w-full  rounded-[10px]
               border border-gray-300 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 "
       >
         <div className="w-full m-4 ">{question.question}</div>
-        <ul className="w-full  mr-4 border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+        <ul className="w-full  mr-2 border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
           <li className=" text-center hover:bg-green-400 ">
             <div className="flex items-center ps-3">
               <input
-                id="list-radio-military"
+                id={question.question + question.answers[0]}
                 type="radio"
                 value="yes"
                 name={question.question}
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                onChange={
+                  () => onChange("yess", question.question, currentQuestionKey)
+                  // onChange(answer, answer_val[i], currentQuestionIndex)
+                }
+                className={`${getBGColor(
+                  userAnswer,
+                  question.answers[0]
+                )}  w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500 `}
+
+                // className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
               />
               <label
-                htmlFor="list-radio-military"
+                htmlFor={question.question + question.answers[0]}
                 className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
               >
                 Тийм
@@ -53,14 +62,19 @@ const CustomCard: React.FC<Props> = ({ question, radioHandler }) => {
           <li className=" text-center hover:bg-green-400">
             <div className="flex items-center ps-3">
               <input
-                id="list-radio-passport1"
+                id={question.question + question.answers[1]}
                 type="radio"
                 value="no"
                 name={question.question}
+                onChange={
+                  () => onChange("nono", question.question, currentQuestionKey)
+                  // onChange(answer, answer_val[i], currentQuestionIndex)
+                }
+                // className={`${getBGColor(userAnswer, answer)}
                 className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
               />
               <label
-                htmlFor="list-radio-passport1"
+                htmlFor={question.question + question.answers[1]}
                 className="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
               >
                 Үгүй
