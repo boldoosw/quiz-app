fb80fc634b26d80cbca874a6577ba4821b19c81b
ganbold <boldoosw@gmail.com>
2024-02-03 17:45:12 +0800

    delgetsh zasaw

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -35,6 +35,7 @@ function AboutCards() {
   // ];
   const cardList = [
     {
+      // src={`/images/climov_images/${answer_pic[i]}`}
       img: card1,
       title: "Түргэн тусламж",
       description: "1. Өөрийг тодорхойлох Түргэн тусламж",
@@ -58,24 +59,22 @@ function AboutCards() {
   ];
   return (
     <>
-      {cardList.map((card, id) => (
-        <div
-          key={id}
-          className="flex flex-col cursor-pointer bg-white justify-center py-6 px-10 text-center items-center mt-12 rounded-tl-[35px] rounded-br-[35px] shadow-2xl md:min-h-[340px] w-full card-item-div max-w-screen-md min-h-[260px]"
-        >
-          <Image
-            src={card.img}
-            alt="box_img"
-            width={200}
-            height={200}
-            className="w-[75px] mb-4"
-          />
-          <p className="text-[24px] font-bold uppercase mb-7">{card.title}</p>
-          <p className="text-[15px] font-medium leading-2 w-full">
-            {card.description}
-          </p>
-        </div>
-      ))}
+      {cardList.map((card, id) =>
+        card != undefined ? (
+          <div
+            key={id}
+            className="flex flex-col cursor-pointer bg-white justify-center py-6 px-10 text-center items-center mt-12 rounded-tl-[35px] rounded-br-[35px] shadow-2xl md:min-h-[340px] w-full card-item-div max-w-screen-md min-h-[260px]"
+          >
+            <Image src={card.img} alt="box_img" className="w-[75px] mb-4" />
+            <p className="text-[16px] font-bold uppercase mb-7">{card.title}</p>
+            <p className="text-[15px] font-medium leading-2 w-full">
+              {card.description}
+            </p>
+          </div>
+        ) : (
+          ""
+        )
+      )}
     </>
   );
 }
